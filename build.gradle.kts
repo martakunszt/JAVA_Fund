79cb79d7e0d3eca50c524af8a33a9978139060b8
martakunsztowicz <kunsztowicz.m@gmail.com>
2024-02-03 11:49:13 +0100

    WebElement findElement By fucntions

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.seleniumhq.selenium:selenium-java:4.12.0")
+
 }
 
 tasks.test {
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,12 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver; // imports Selenium webdriver from dependencies
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver; //adds chrome webdriver
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
        WebDriver driver = new ChromeDriver(); //starts internet browser
-       driver.get("file:///Users/martakunsztowicz/Desktop/Postgraduate/examplePage/index.html"); //void procedure
+       driver.get("file:///Users/martakunsztowicz/IdeaProjects/Java_Fund/src/test/resources/index.html"); //void procedure
         System.out.println("Webpage address is :" + driver.getCurrentUrl());
         System.out.println("The title of the page is: " + driver.getTitle());
         //driver.quit(); //void procedure -> no output
@@ -15,5 +16,31 @@ public class Main {
         WebElement firstParagraph = driver.findElement(By.tagName("p")); //getting text
         String paraText = firstParagraph.getText(); //since p is a String, then we need to get this
         System.out.println(paraText);
+
+        List<WebElement> listP = driver.findElements(By.tagName("p")); //will print all paragraphs
+       for (WebElement i:listP) { //i - iterating on a list, basically 'i' is just a variable that iterates on our listP and sout first p, than it does the same thing on the other 'p'
+           System.out.println(i.getText()); //this prints every paragraph from the listP
+       }
+
+       WebElement thirdParagraph = driver.findElement(By.name("newUniqueName")); //find element by name attribute
+        System.out.println("This is our third paragraph, find by name attribute: "  + thirdParagraph.getText());
+
+        WebElement magentaParagraph = driver.findElement(By.id("firstParagraph")); //find element by id attribute
+        System.out.println("This is our first magenta paragraph, find by id attribute: " + magentaParagraph.getText());
+
+        WebElement secondParagraph = driver.findElement(By.className("paragraph"));
+        System.out.println("This is our second paragraph, find element by class: " + secondParagraph.getText() ); //find by class attribute
+
+        WebElement headingXpath = driver.findElement(By.xpath("/html/body/h2")); //finding element by xPath absolute attribute
+        System.out.println("This is header found by xpath attribute: " + headingXpath.getText());
+
+        WebElement relXpath = driver.findElement(By.xpath("//h2[@class='relativePath']")); //finding element by xPath relative attribute
+        System.out.println("This is header found by xpath relative attribute: " + relXpath.getText());
+
+        //WebElement myAtt = driver.findElement(By.cssSelector("h5[data-test-id='heading']"));
+        //System.out.println("Finding element using my own HTML attribute: " + myAtt.getText()); //for some reason this doesn't work
+
+        driver.quit();
+
     }
 }

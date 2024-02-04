d783b3e3de4582f0dcf90c95c782731065e3d838
martakunsztowicz <kunsztowicz.m@gmail.com>
2024-02-04 13:23:37 +0100

    using WebDriverManager clicking through menu

---
+++ src/test/java/Epl.java
@@ -0,0 +1,56 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class Epl {
+    private static WebDriver driver;
+    String sut = "http://e.pl";
+    @BeforeAll
+    static void setupClass(){
+        WebDriverManager.chromedriver().setup(); //resolves any conflicts with drivers and browser's version
+        System.out.println("Driver found");
+    }
+
+    @BeforeEach
+    void setup(){
+        driver = WebDriverManager.chromedriver().create(); //creates browser instance
+    }
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+    }
+
+   // @AfterAll //used in big projects
+
+    @Test
+    @Order(2)
+    public void openPage() {
+        driver.getTitle();
+        System.out.println("Welcome to the e.pl page");
+        System.out.println("Second test: finished");
+    }
+
+    // clicking on the menu
+    @Test
+    @Order(1)
+    public void clickMenu(){
+        driver.get(sut);
+
+        WebElement emailButton = driver.findElement(By.cssSelector("a[title=Email]"));
+        emailButton.click();
+        System.out.println("Page title: " + driver.getTitle());
+
+       WebElement domainButton = driver.findElement(By.cssSelector("a[title=Domeny]"));
+        domainButton.click();
+        System.out.println("Page title: " + driver.getTitle());
+
+        WebElement hostButton = driver.findElement(By.cssSelector("a[title=Hosting]"));
+        hostButton.click();
+        System.out.println("Page title: " + driver.getTitle());
+        System.out.println("First test: finished");
+    }
+}
